c3c6488103b1b343ae4d37d97c323a9e55b78b83
Karly Mareka <karly.mareka@gmail.com>
2019-07-02 02:20:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,8 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  get '/newteam' do 
+  get '/newteam' do
+    @team_name = params["name"]
     erb :newteam
     @team_name = params["name"]
   end
